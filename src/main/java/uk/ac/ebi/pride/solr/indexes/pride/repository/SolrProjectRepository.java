ed2329752f4f0239dd897112625a7059225b3e5d
Yasset Perez-Riverol <ypriverol@gmail.com>
2018-05-04 22:44:32 +0100

    minor changes

--- src/main/java/uk/ac/ebi/pride/solr/indexes/pride/repository/SolrProjectRepository.java
+++ src/main/java/uk/ac/ebi/pride/solr/indexes/pride/repository/SolrProjectRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.solr.core.query.result.FacetAndHighlightPage;
 import org.springframework.data.solr.repository.Facet;
+import org.springframework.data.solr.repository.Highlight;
 import org.springframework.data.solr.repository.Query;
 import uk.ac.ebi.pride.solr.indexes.pride.model.PrideProjectField;
 import uk.ac.ebi.pride.solr.indexes.pride.model.PrideSolrDataset;
@@ -49,6 +50,7 @@ public interface SolrProjectRepository extends SolrProjectRepositoryCustom, Crud
             PrideProjectField.PROJECT_UPDATED_DATE, PrideProjectField.PROJECT_TAGS_FACET, PrideProjectField.PROJECT_KEYWORDS_FACET,
             PrideProjectField.INSTRUMENTS_FACET, PrideProjectField.AFFILIATIONS_FACET, PrideProjectField.COUNTRIES_FACET, PrideProjectField.EXPERIMENTAL_FACTORS_FACET,
             PrideProjectField.ORGANISMS, PrideProjectField.ORGANISMS_PART, PrideProjectField.DISEASES})
+    @Highlight(fields = {PrideProjectField.PROJECT_TILE, PrideProjectField.PROJECT_DESCRIPTION})
     FacetAndHighlightPage<PrideSolrDataset> findAllWithFacetIgnoreCase(Pageable pageable);
 
 }
